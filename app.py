49f6757939a35bc8826a2cce7abcd89b1a2fc7d4
Michał Nerwiński <42096347+n3rwus@users.noreply.github.com>
2022-10-14 19:52:18 +0200

    Testing pytroch

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        return jsonify({'test': 'test_result'})
 
 
 if __name__ == '__main__':
