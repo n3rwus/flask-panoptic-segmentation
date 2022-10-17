6162c74223ab9e250fade148819b4371e3e48da7
Michał Nerwiński <42096347+n3rwus@users.noreply.github.com>
2022-10-17 20:52:15 +0200

    Refactoring config files

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
-import datetime
-
-from flask import Flask, jsonify, request, render_template
-from flask_pymongo import PyMongo
+from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-
-
-@app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return render_template("index.html",
-                           online_users=online_users)
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # 1 load image
-    # 2 image -> tensor
-    # 3 prediction
-    # 4 return json
-    if request.method == 'POST':
-        return jsonify({'result': 1})
+    return "<h1>DUPA</h1>"
 
 
 if __name__ == '__main__':
     app.run()
-
-# https://www.mongodb.com/developer/languages/python/flask-python-mongodb/
