5ea39a7421758080f3680c1e81e7a8fe6dd9ba8c
Michał Nerwiński <42096347+n3rwus@users.noreply.github.com>
2022-11-13 15:45:02 +0100

    Removing unused files

--- application/routes.py
+++ application/routes.py
@@ -5,77 +5,9 @@ from application.inference import get_prediction
 from application.transform_pipeline import format_class_name
 
 
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def index():
-    return render_template("index.html", index=True)
-
-
-@app.route('/login')
-def login():
-    return render_template("login.html", login=True)
-
-
-@app.route('/courses/')
-@app.route('/courses/<term>')
-def courses(term='Spring 2019'):
-    courses_data = [
-        {
-            "courseID": "1111",
-            "title": "PHP 102",
-            "description": "Intro to PHP",
-            "credits": 3,
-            "term": "Fall, Spring"
-        },
-        {
-            "courseID": "2222",
-            "title": "Java 1",
-            "description": "Intro to Java Programming",
-            "credits": 4,
-            "term": "Spring"
-        },
-        {
-            "courseID": "3333",
-            "title": "Adv PHP 201",
-            "description": "Advanced PHP Programming",
-            "credits": 3,
-            "term": "Fall"
-        },
-        {
-            "courseID": "4444",
-            "title": "Angular 1",
-            "description": "Intro to Angular",
-            "credits": 3,
-            "term": "Fall, Spring"
-        },
-        {
-            "courseID": "5555",
-            "title": "Java 2",
-            "description": "Advanced Java Programming",
-            "credits": 4,
-            "term": "Fall"
-        }
-    ]
-    return render_template("courses.html", courseData=courses_data, courses=True, term=term)
-
-
-@app.route('/register')
-def register():
-    return render_template("register.html", register=True)
-
-
 @app.route('/about')
 def about():
     return render_template("about.html", about=True)
-
-
-@app.route('/enrollment')
-def enrollment():
-    course_id = request.args.get('courseID')
-    title = request.args.get('title')
-    term = request.args.get('term')
-    return render_template("enrollment.html", enrollment=True, data={"id": course_id, "title": title, "term": term})
 
 
 @app.route('/predict', methods=['POST'])
@@ -87,7 +19,9 @@ def predict():
         return jsonify({'class_id': class_id, 'class_name': class_name})
 
 
-@app.route('/tmp', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -100,4 +34,4 @@ def upload_file():
         class_name = format_class_name(class_name)
         return render_template('result.html', class_id=class_id,
                                class_name=class_name)
-    return render_template('tmp.html')
+    return render_template('index.html')
