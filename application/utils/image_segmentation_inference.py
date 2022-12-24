856d1a4964028b5006c18690d9c4090fb16bff18
n3rwus <mnerwinski@outlook.com>
2022-12-24 15:08:01 +0000

    print panoptic segmentation

--- application/utils/image_segmentation_inference.py
+++ application/utils/image_segmentation_inference.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 from image_segmentation_transform_pipeline import transform_image, get_model, postprocessor, get_tensor
 import itertools
 import seaborn as sns
+import numpy
+import io
+from PIL import Image
+import panopticapi
+from panopticapi.utils import id2rgb, rgb2id
+
 
 palette = itertools.cycle(sns.color_palette())
 model = get_model()
@@ -11,7 +17,7 @@ tensor = get_tensor()
 out = model(tensor)
 
 
-def print_panoptic_segmentation():
+def get_postprocessor_result():
     return postprocessor(out, torch.as_tensor(
         tensor.shape[-2:]).unsqueeze(0))[0]
 
@@ -36,3 +42,22 @@ def print_remaining_masks():
         ax.imshow(mask, cmap="cividis")
         ax.axis('off')
     return fig.tight_layout()
+
+
+def print_panoptic_segmentation():
+    # The post-processor expects as input the target size of the predictions (which we set here to the image size)
+    result = get_postprocessor_result()
+    # The segmentation is stored in a special-format png
+    panoptic_seg = Image.open(io.BytesIO(result['png_string']))
+    panoptic_seg = numpy.array(panoptic_seg, dtype=numpy.uint8).copy()
+    # Retrieve the ids corresponding to each mask
+    panoptic_seg_id = rgb2id(panoptic_seg)
+    # Color each mask individually
+    panoptic_seg[:, :, :] = 0
+    for ID in range(panoptic_seg_id.max() + 1):
+        panoptic_seg[panoptic_seg_id == ID] = numpy.asarray(
+            next(palette)) * 255
+    plt.figure(figsize=(15, 15))
+    plt.imshow(panoptic_seg)
+    plt.axis('on')
+    return plt.show()
