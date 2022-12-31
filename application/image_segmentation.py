e4dd74a6c3464629b876c6dae70c69eba89354c5
n3rwus <mnerwinski@outlook.com>
2022-12-31 00:18:06 +0000

    removing unused func

--- application/image_segmentation.py
+++ application/image_segmentation.py
@@ -4,69 +4,14 @@ import cv2
 import math
 import numpy
 import torch
-import requests
 import itertools
-import panopticapi
-from torch import nn
 from PIL import Image
 import seaborn as sns
-import IPython.display
 from copy import deepcopy
 import matplotlib.pyplot as plt
-from detectron2.config import get_cfg
-from torchvision.models import resnet50
-import torchvision.transforms as transform
 from detectron2.data import MetadataCatalog
-from panopticapi.utils import id2rgb, rgb2id
+from panopticapi.utils import rgb2id
 from detectron2.utils.visualizer import Visualizer
-
-
-# torch.set_grad_enabled(False)
-
-# model, postprocessor = torch.hub.load('facebookresearch/detr', 'detr_resnet101_panoptic', pretrained=True,
-#                                       return_postprocessor=True, num_classes=250)
-
-
-# def get_model():
-#     return model.to(torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')).eval()
-
-
-# def read_coco_classes_from_file(path='application/static/coco_classes.txt'):
-#     coco_classes_file = open(path, "r")
-
-#     coco_classes = coco_classes_file.read()
-#     coco_classes_file.close()
-#     return coco_classes.split(",")
-
-
-# Detectron2 uses a different numbering scheme, we build a conversion table
-# def conversion_table(CLASSES):
-#     coco2d2 = {}
-#     count = 0
-#     for i, c in enumerate(CLASSES):
-#         if c != "N/A":
-#             coco2d2[i] = count
-#             count += 1
-
-
-# coco2d2 = conversion_table(read_coco_classes_from_file())
-
-
-# standard PyTorch mean-std input image normalization
-# def transform_image(image_bytes):
-#     transform = transform.Compose([
-#         transform.Resize(800),
-#         transform.ToTensor(),
-#         transform.Normalize(
-#             [0.485, 0.456, 0.406],
-#             [0.229, 0.224, 0.225])
-#     ])
-#     image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-#     tensor = transform(image).unsqueeze(0)
-
-#     if torch.cuda.is_available():
-#         tensor = tensor.to(torch.device("cuda:0"))
-#     return model(tensor)
 
 
 # compute the scores, excluding the "no-object" class (the last one)
--- application/routes.py
+++ application/routes.py
@@ -1,16 +1,12 @@
 import io
-import base64
 import torch.cuda
 from PIL import Image
-from io import BytesIO
-from matplotlib.figure import Figure
 
 from application import app
 from application.image_segmentation import print_detectron2_visualization, print_panoptic_segmentation, print_remaining_masks
-from flask import render_template, request, jsonify, redirect
+from flask import render_template, request, redirect
 
-from application.inference import get_prediction, get_segmentation
-from application.transform_pipeline import format_class_name
+from application.inference import get_segmentation
 
 
 @app.route('/about')
@@ -18,26 +14,10 @@ def about():
     return render_template("about.html", about=True)
 
 
-# @app.route('/test', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         if 'file' not in request.files:
-#             return redirect(request.url)
-#         file = request.files.get('file')
-#         if not file:
-#             return redirect(request.url)
-#         img_bytes = file.read()
-#         class_id, class_name = get_prediction(image_bytes=img_bytes)
-#         class_name = format_class_name(class_name)
-#         return render_template('result.html', class_id=class_id,
-#                                class_name=class_name, image_after=img_bytes, is_cuda_used=torch.cuda.is_available())
-#     return render_template('index.html')
-
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
-def panoptic_image_segmentation():
+def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
