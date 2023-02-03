c0671480c88b77af108317eaca64242bc4f7a634
n3rwus <mnerwinski@outlook.com>
2023-02-03 22:06:23 +0000

    refractoring

--- application/__init__.py
+++ application/__init__.py
@@ -19,6 +19,7 @@ def create_app():
 
     from .models import User
 
+    @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
